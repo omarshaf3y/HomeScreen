3841f9c9ba84b1843ede0988e71d84a48c65391d
omarshaf3y <omarshaf3y99@gmail.com>
2024-07-30 20:31:21 +0300

    create custom app bar

--- lib/features/home/presentations/views/home_view.dart
+++ lib/features/home/presentations/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/custom_app_bar.dart';
 import 'widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -7,7 +8,8 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
+      appBar: CustomAppBar(),
       body: HomeViewBody(),
     );
   }
---
+++ lib/features/home/presentations/views/widgets/custom_app_bar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: AppBar(
+        title: Image.asset(
+          'assets/images/E-shop 1.png',
+        ),
+        actions: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: const Color(0xffE2E8F0),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(10)),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.search,
+                    size: 25,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: const Color(0xffE2E8F0),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.notifications_active_outlined,
+                    size: 25,
+                    color: Color(0xff0FA958),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
